7679f3c36476ad3dc3330949952598e82f9be993
Miguel Ontiveros <ontiverosmiguel24@gmail.com>
2021-12-13 23:00:47 -0800

    add message

---
+++ starting-react/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+  <div
+    style={{
+      margin: "auto",
+      width: 800,
+      paddingTop: "1rem",
+    }}
+    >
+    <h1 className="title">Pokemon Search</h1>
+    <table width="100%">
+      <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Bulbasaur</td>
+          <td>Grass, Poison</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  );
+}
+
+export default App;
